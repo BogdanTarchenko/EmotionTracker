6a0a247c6a70828c78e65c1cb753aa24ce79c98a
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2025-02-21 19:14:02 +0700

    Add WelcomeTitle

--- EmotionTracker/Presentation/Welcome/WelcomeViewController.swift
+++ EmotionTracker/Presentation/Welcome/WelcomeViewController.swift
@@ -32,8 +32,24 @@ private extension WelcomeViewController {
         view.addSubview(appleIDButton)
         
         appleIDButton.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(Metrics.appleIDButtonHorizontalEdgesInset)
+            make.horizontalEdges.equalToSuperview().inset(Metrics.horizontalEdgesInset)
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(Metrics.appleIDButtonBottomInset)
+        }
+        
+        let welcomeTitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = Constants.welcomeTitle
+            label.numberOfLines = Metrics.welcomeTitleLabelNumberOfLines
+            label.textColor = Constants.titleTextColor
+            label.font = Constants.titleFont
+            return label
+        }()
+        
+        view.addSubview(welcomeTitleLabel)
+        
+        welcomeTitleLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(Metrics.horizontalEdgesInset)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(Metrics.welcomeTitleLabelTopInset)
         }
     }
 }
@@ -56,11 +72,16 @@ private extension WelcomeViewController {
             (-0.5, 1.5, 1400)
         ]
         
-        static let appleIDButtonHorizontalEdgesInset: CGFloat = 24
+        static let horizontalEdgesInset: CGFloat = 24
         static let appleIDButtonBottomInset: CGFloat = 24
+        static let welcomeTitleLabelTopInset: CGFloat = 24
+        static let welcomeTitleLabelNumberOfLines: Int = 2
     }
     
     enum Constants {
         static let appleIDButtonTitle: String = LocalizedKey.Welcome.appleIDButtonTitle
+        static let welcomeTitle: String = LocalizedKey.Welcome.welcomeTitle
+        static let titleTextColor: UIColor = UIColor.textSecondary
+        static let titleFont = UIFont.systemFont(ofSize: 48, weight: .bold)
     }
 }
